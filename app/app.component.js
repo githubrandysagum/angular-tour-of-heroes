eff22135a85ecdd2b48dd4a7f4051f8f5583dfc7
Randy Sagum <randy.sagum@yahoo.com>
2016-10-17 03:04:28 -0700

    done routing

--- app/app.component.js
+++ app/app.component.js
@@ -18,7 +18,8 @@ var AppComponent = (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'my-app',
-            template: "<h1>{{title}}</h1>\n                <nav>\n                    <a routerLink=\"/dashboard\">Dashboard</a>\n                    <a routerLink=\"/heroes\">Heroes</a>\n                </nav>\n                <router-outlet></router-outlet>\n              "
+            template: "\n        <h1>{{title}}</h1>\n        <nav>\n            <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Dashboard</a>\n            <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a>\n        </nav>\n        <router-outlet></router-outlet>\n    ",
+            styleUrls: ['app.component.css']
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
